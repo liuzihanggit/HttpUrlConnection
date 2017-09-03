dcd4d3e093d767cad39024b30da0b44d7e1ebd63
liuzihang <1390712673@qq.com>
2017-09-03 14:39:13 +0800

    First commit

---
+++ app/src/main/java/liunian/httpurlconnection/MainActivity.java
@@ -0,0 +1,182 @@
+package liunian.httpurlconnection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView responseText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.send_request);
+        responseText = (TextView)findViewById(R.id.response_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view.getId() == R.id.send_request){
+                    //sendRequestWithHttpUrlConnection();
+                    sendRequestwithOkhttp();
+                }
+            }
+        });
+
+    }
+    private void sendRequestwithOkhttp(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    //Toast.makeText(MainActivity.this,"调用函数11",Toast.LENGTH_LONG).show();
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://192.168.191.1/get_data.xml")
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+
+                    parseXMLWithPull(responseData);
+                    //showResponse(responseData);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    private void parseXMLWithPull(String xmlData){
+        try{
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+
+
+
+
+            String id = "";
+            String name = "";
+            String version = "";
+
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                //responseText.setText(eventType +"first"+"  ");
+
+                String nodeName = xmlPullParser.getName();
+
+
+                switch (eventType){
+                    //开始解析某个节点
+                    case XmlPullParser.START_TAG:{
+                        if("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                            //responseText.setText(id);
+                        }else if("name".equals(nodeName)){
+                            name = xmlPullParser.nextText();
+                        }else if("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+
+                    //完成解析某个节点
+                    case XmlPullParser.END_TAG:{
+                        if ("app".equals(nodeName)){
+                            //responseText.setText(id);
+                            Log.d("MainActivity","id is " +id );
+                            Log.d("MainActivity","name is "+name);
+                            Log.d("MainActivity","version is"+version);
+                        }
+                        break;
+                    }
+                    default:
+                        //responseText.setText(nodeName +"nihao");
+                        break;
+
+                }
+
+                eventType = xmlPullParser.next();
+
+
+
+
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    /*
+    private void sendRequestWithHttpUrlConnection(){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+
+                try{
+                    URL url = new URL("https://www.baidu.com");
+                    connection = (HttpURLConnection)url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+
+                    //下面对获取到的输入流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine())!= null){
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(reader != null){
+                        try{
+                            reader.close();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                        if(connection != null){
+                            connection.disconnect();
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+*/
+    private void showResponse(final String response){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                responseText.setText(response);
+            }
+        });
+    }
+}
